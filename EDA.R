f04dfb30b59410291a2dd9a62e12ebde7622889c
Edward <32278593+evalentine88@users.noreply.github.com>
2017-12-06 11:50:36 -0500

    Update to utility and EDA scripts

--- EDA.R
+++ EDA.R
@@ -1,1 +1,45 @@
 # Useful functions for exploratory data analysis (EDA)
+plot_missing <- function(data, title = NULL){
+    temp <- as.data.frame(ifelse(is.na(data), 0, 1))
+    temp <- temp[,order(colSums(temp))]
+    plot_data <- expand.grid(list(x = 1:nrow(temp), 
+                                  y = colnames(temp)))
+    plot_data$m <- as.vector(as.matrix(temp))
+    plot_data <- data.frame(x = unlist(plot_data$x), 
+                            y = unlist(plot_data$y), 
+                            m = unlist(plot_data$m))
+    ggplot(plot_data,
+           aes(x=x, y=y, fill=factor(m))) + 
+        geom_tile() + 
+        scale_fill_manual(values=c("white", "black"), 
+                          name="Missing\n(0=Yes, 1=No)") + 
+        theme_light() + 
+        ylab("") + 
+        xlab("") + 
+        ggtitle(title)
+}
+
+missing_corr_plot <- function(data, outcome, title = NULL) {
+    temp <- data.frame(x = rowSums(sapply(data, is.na)),
+                       y = data[ , outcome])
+    plot_data <- temp %>%
+        group_by(x) %>%
+        summarise(y = mean(y),
+                  n = n())
+    ggplot(plot_data, aes(x = x, y = y, size = n)) +
+        geom_point() +
+        geom_smooth(show.legend = F,
+                    se = F) +
+        scale_size_continuous(name = "Total\nObservations") +
+        ggtitle(title) +
+        theme_bw() 
+}
+
+color_plot <- function(data, x, y, color) {
+    temp <- data %>%
+        select_(x, y, color) %>%
+        na.omit() %>%
+        ggplot(aes_string(x = x, y = y, color = color)) +
+        geom_smooth() +
+        theme_bw()
+}
--- ev_utils.R
+++ ev_utils.R
@@ -1,1 +1,76 @@
 # Utility Functions
+load_packages <- function(package_vector, edward = FALSE) {
+    if (edward == TRUE) {
+        package_vector <- c("tidyr", "dplyr", "ggplot2", 
+                            "GGally", "lubridate", "foreach",
+                            "doParallel", "xgboost", "Matrix",
+                            "caret", "forecast", "RODBC")
+    }
+    for (i in package_vector) {
+        if(!require(i, character.only=TRUE)) install.packages(i)
+        require(i, character.only = TRUE)
+    }
+}
+
+# Round to the nearest multiple
+mround <- function(x,base){
+    base*round(x/base)
+}
+
+# Used for quickly manipulating columns based on a pattern
+grepl_column <- function(pattern, data) {
+    colnames(data)[grepl(pattern, colnames(data))]
+}
+
+# Move columns based on specified location
+moveMe <- function(data, tomove, where = "last", ba = NULL) {
+    temp <- setdiff(names(data), tomove)
+    x <- switch(
+        where,
+        first = data[c(tomove, temp)],
+        last = data[c(temp, tomove)],
+        before = {
+            if (is.null(ba)) stop("must specify ba column")
+            if (length(ba) > 1) stop("ba must be a single character string")
+            data[append(temp, values = tomove, after = (match(ba, temp)-1))]
+        },
+        after = {
+            if (is.null(ba)) stop("must specify ba column")
+            if (length(ba) > 1) stop("ba must be a single character string")
+            data[append(temp, values = tomove, after = (match(ba, temp)))]
+        })
+    x
+}
+
+# Rename a vector of columns
+col_rename <- function(x, ...) {
+    options(error = NULL)
+    input_list <- list(...)
+    if (all(unlist(lapply(input_list, function(x) is.character(x)))) == FALSE) {
+        stop("Input must be quoted strings, not objects")
+    }
+    for (i in 1:length(input_list)) {
+        if(any(colnames(x) == names(input_list)[i]) == FALSE) {
+            stop(paste0("The column \"",
+                        names(input_list)[i], 
+                        "\" does not match any of the columns in the specified data frame"))
+        }
+        colnames(x)[colnames(x) == names(input_list)[i]] <- input_list[[i]]
+    }
+    return(x)
+}
+
+# Set NA elements to a specified value
+set_na <- function(data, columns, value) {
+    temp <- data
+    for (i in  columns) {
+        t_parse <- paste0("ifelse(is.na(", "x", "),",
+                          value, ",",
+                          "x", ")")
+        temp <- temp %>%
+            mutate_(.dots = setNames(lazyeval::interp(t_parse, 
+                                                      x = as.name(i)), 
+                                     i))
+    }
+    return(temp)
+}
